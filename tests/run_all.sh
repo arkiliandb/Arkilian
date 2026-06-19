79c3064e3724ddba6a56eb928d17b4199184e911
fridaycandour <fridaymichaels662@gmail.com>
2026-06-19 04:53:03 +0100

    test: add hydration and server test suites to run_all.sh

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -11,9 +11,15 @@
                                                        -Isrc -Isrc/deps/sqlite -lcurl -lpthread -o test_interception
  ./test_interception
 
+ # Test hydration
  cc tests/test_hydration.c src/hydration.c \
                                     -Isrc -Isrc/deps/sqlite -lcurl -lsqlite3 -o test_hydration
  ./test_hydration
+
+ # Test server
+ cd server
+ go test -v ./...
+ cd ..
 
  # Test benchmark with 1M insertions
  cc -O2 tests/bench_1m.c src/class.c src/deps/sqlite/sqlite3.c \
